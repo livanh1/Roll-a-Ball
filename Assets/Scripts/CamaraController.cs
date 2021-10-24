2bb23391c70b8bbb76c2723968b480e62442f2b4
Livan Herrera <livanh1@hotmail.com>
2021-10-24 09:54:46 -0600

    proyecto roll a ball

---
+++ Assets/Scripts/CamaraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraController : MonoBehaviour
+{
+    public GameObject jugador;
+
+    private Vector3 offset;
+    void Start()
+    {
+        offset = transform.position - jugador.transform.position;
+        
+    }
+
+    void LateUpdate()
+    {
+        transform.position = jugador.transform.position + offset;
+        
+    }
+}
---
+++ Assets/Scripts/JugadorController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JugadorController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private int contador;
+    public float velocidad;
+    public Text TextoContador, TextoGanar;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        contador = 0;
+        setTextoContador();
+        TextoGanar.text="";
+    }
+
+    void FixedUpdate ()
+    {
+        float movimientoH = Input.GetAxis ("Horizontal");
+        float movimientoV = Input.GetAxis ("Vertical");
+        Vector3 movimiento = new Vector3 (movimientoH, 0.0f,movimientoV);
+        rb.AddForce(movimiento * velocidad);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("coleccionable"))
+        {
+            other.gameObject.SetActive(false);
+            contador = contador + 1;
+            setTextoContador();
+        }
+    }
+        void setTextoContador()
+        {
+            TextoContador.text = "Contador: " + contador.ToString();
+            if (contador >= 12){
+            TextoGanar.text = "¡Ganaste!";
+        }
+    }
+}
